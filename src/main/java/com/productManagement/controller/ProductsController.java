220a0d663d3b77a839b67415cea3be3a422da016
Oleg <kovalyovop@yandex.ru>
2024-05-17 22:39:04 +0300

    17/05/2024 дополнение к предыдущему

--- src/main/java/com/productManagement/controller/ProductsController.java
+++ src/main/java/com/productManagement/controller/ProductsController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -29,9 +30,19 @@ public class ProductsController {
     }
 
     @PostMapping("create")
-    public String createProduct(@Valid NewProductPayload payload, BindingResult bindingResult) {
-        Product product = this.productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String createProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors()
+                    .stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+        }
     }
-
 }
