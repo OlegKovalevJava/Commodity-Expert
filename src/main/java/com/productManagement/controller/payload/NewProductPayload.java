521f7ad881a326953f60bc411128f9c071fadfb2
Oleg <kovalyovop@yandex.ru>
2024-05-23 19:12:43 +0300

    23/05/2024 +

--- src/main/java/com/productManagement/controller/payload/NewProductPayload.java
+++ src/main/java/com/productManagement/controller/payload/NewProductPayload.java
@@ -4,10 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record NewProductPayload(
-        @NotNull
-        @Size(min = 3, max = 100, message = "{catalogue.products.create.errors.title_is_invalid}")
+        @NotNull(message = "{catalogue.products.create.errors.title_is_null}")
+        @Size(min = 3, max = 50, message = "{catalogue.products.create.errors.title_size_is_invalid}")
         String title,
-
-        @Size(max = 500, message = "{catalogue.products.create.errors.title_is_invalid}")
+        @Size(max = 1000, message = "{catalogue.products.create.errors.details_size_is_invalid}")
         String details) {
 }
